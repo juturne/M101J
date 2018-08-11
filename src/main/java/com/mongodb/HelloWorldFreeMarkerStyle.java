a0379808f022233c0e4e72ea2dee71c445a82b5e
Su Tasbas <tasbas.su@gmail.com>
2018-08-11 18:25:25 +0100

    Week 1: Freemarker Templating Engine

---
+++ src/main/java/com/mongodb/HelloWorldFreeMarkerStyle.java
@@ -0,0 +1,30 @@
+package com.mongodb;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloWorldFreeMarkerStyle {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_27);
+        configuration.setClassForTemplateLoading(HelloWorldFreeMarkerStyle.class, "/");
+
+        try {
+            Template helloTemplate = configuration.getTemplate("hello.ftl");
+            StringWriter writer = new StringWriter();
+            Map<String, Object> helloMap = new HashMap<>();
+            helloMap.put("name", "Freemarker");
+
+            helloTemplate.process(helloMap, writer);
+
+            System.out.println(writer);
+        } catch (IOException | TemplateException e) {
+            e.printStackTrace();
+        }
+    }
+}
