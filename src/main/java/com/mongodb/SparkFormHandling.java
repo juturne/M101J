ee3593fe79fe2e78ecc811b3540e474bb719c89f
Su Tasbas <tasbas.su@gmail.com>
2018-08-11 19:05:35 +0100

    Week 1: Handling POST Requests

---
+++ src/main/java/com/mongodb/SparkFormHandling.java
@@ -0,0 +1,59 @@
+package com.mongodb;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.halt;
+
+public class SparkFormHandling {
+
+    public static void main(String[] args) {
+        // Configure Freemarker
+        final Configuration configuration = new Configuration(Configuration.VERSION_2_3_27);
+        configuration.setClassForTemplateLoading(SparkFormHandling.class, "/");
+
+        // Configure routes
+        Spark.get("/", new Route() {
+            @Override
+            public Object handle(final Request request, final Response response) {
+                try {
+
+                    Map<String, Object> fruitsMap = new HashMap<>();
+                    fruitsMap.put("fruits",
+                            Arrays.asList("Mango", "Coconut", "Banana", "Dragon Fruit"));
+
+                    Template fruitPickerTemplate =
+                            configuration.getTemplate("fruitPicker.ftl");
+                    StringWriter writer = new StringWriter();
+                    fruitPickerTemplate.process(fruitsMap, writer);
+                    return writer;
+
+                } catch (Exception e) {
+                    halt(500);
+                    return null;
+                }
+            }
+        });
+
+        Spark.post("/favorite_fruit", new Route() {
+            @Override
+            public Object handle(final Request request, final Response response) {
+                final String fruit = request.queryParams("fruit");
+                if (fruit == null) {
+                    return "Why don't you pick one?";
+                } else {
+                    return "Your favorite fruit is " + fruit;
+                }
+            }
+        });
+    }
+}
