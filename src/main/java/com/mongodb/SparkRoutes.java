2250dd3c4a61d1d887305d82210ceb9b82d0d563
Su Tasbas <tasbas.su@gmail.com>
2018-08-11 18:49:49 +0100

    Week 1: Handling GET Requests

---
+++ src/main/java/com/mongodb/SparkRoutes.java
@@ -0,0 +1,31 @@
+package com.mongodb;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+public class SparkRoutes {
+    public static void main(String[] args) {
+        Spark.get("/", new Route() {
+            @Override
+            public Object handle(final Request request, final Response response) {
+                return "Hello World\n";
+            }
+        });
+
+        Spark.get("/test", new Route() {
+            @Override
+            public Object handle(final Request request, final Response response) {
+                return "This is a test page\n";
+            }
+        });
+
+        Spark.get("/echo/:thing", new Route() {
+            @Override
+            public Object handle(final Request request, final Response response) {
+                return request.params(":thing");
+            }
+        });
+    }
+}
